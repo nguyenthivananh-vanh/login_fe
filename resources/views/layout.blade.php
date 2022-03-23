5b07ba8666123e589fe9c021927436d2783d5d23
myhoa99 <nguyenthimyhoa28999@gmail.com>
2022-03-23 15:50:35 +0700

    create project

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>KiotViet</title>
+        <base href="{{ asset('') }}">
+     
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+        integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">       
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <!-- Styles -->
+        {{-- <link rel="stylesheet" href="{{URL::asset('css/main.css')}}"> --}}
+        
+       <link href="assets/css/main.css" rel="stylesheet">
+        <style>
+            body {
+                font-family: Arial,Helvetica,sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+@section('content')
+    <div class="wrapper">
+        <div class="login__bg mainWrap container-fluid"> 
+            <div class="form__login container">
+                <form action="">
+                    <div class="form__login__card">
+                        <div class="card__header">
+                            <div class="row">                          
+                                <div class="col-12">
+                                    <span>Đăng nhập</span>
+                                    <img src="https://cdn-app.kiotviet.vn/retailler/Content/kiotvietLogo.png" alt="KiotViet" title="KiotViet">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card__body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" class="card__body__input" id="" placeholder="Tên đăng nhập" >                                      
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="card__body__input" id="" placeholder="Mật khẩu">
+                                    </div>
+                                    <div class="form-group d-flex align-items-center" >
+                                        <input type="checkbox" class="" id="" >
+                                        <span class="card__body__check">Duy trì đăng nhập</span>
+                                        <div class="card__body__split">
+                                            <span>|</span>                                      
+                                            <a href="">Quên mật khẩu?</a>
+                                        </div>                                       
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card__footer">
+                            <div class="row" >
+                                <div class="col-6" style="padding: 0">
+                                    <button class="card__footer__btn--blue"> <i class="fa-solid fa-chart-line" style="width: 20px;"></i> Quản lý</button>
+                                </div>
+                                <div class="col-6" style="padding: 0">
+                                    <button class="card__footer__btn--green"> <i class="fas fa-shopping-basket"></i> Bán hàng </button>
+                                </div>
+                            </div>                          
+                        </div>                                           
+                    </div>
+                </form>
+                
+            </div>           
+        </div>
+    </div>
+   
+@endsection
